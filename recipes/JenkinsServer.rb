7c9b044f2fbc04f382a94de842d1feb1009a7941
Arjen de Rijke <arjenderijke@gmail.com>
2016-04-19 10:34:27 +0200

    Enable authentification in the jenkins web ui. This closes #7

--- recipes/JenkinsServer.rb
+++ recipes/JenkinsServer.rb
@@ -6,7 +6,44 @@
 package 'emacs'
 package 'git'
 
+# Setup authentification
+ssh_private_key_file = File.join(Chef::Config[:file_cache_path], 'cookbooks/aws_chef_jenkins/files/default/id_rsa')
+ssh_private_key = File.read(ssh_private_key_file)
+ssh_public_key_file = File.join(Chef::Config[:file_cache_path], 'cookbooks/aws_chef_jenkins/files/default/id_rsa.pub')
+ssh_public_key = File.read(ssh_public_key_file)
+node.run_state[:jenkins_private_key] = ssh_private_key
+
 include_recipe 'jenkins::master'
+
+# Create the Jenkins user with the public key
+jenkins_user 'Chef' do
+  public_keys [ssh_public_key]
+end
+
+secretsfilename = File.join(Chef::Config[:file_cache_path], 'cookbooks/aws_chef_jenkins/files/default/', node['secretsfilename'])
+secretsfile = File.read(secretsfilename)
+secretsobject = JSON.parse(secretsfile)
+
+jenkins_user 'admin' do
+  full_name 'Admin'
+  email     secretsobject['recipients']
+  password  secretsobject['admin-password']
+end
+
+template "#{node['jenkins']['master']['home']}/config.xml" do
+  source 'default/config.xml'
+  notifies :reload, 'service[jenkins]', :delayed
+end
+
+template "#{node['jenkins']['master']['home']}/jenkins.model.DownloadSettings.xml" do
+  source 'default/jenkins.model.DownloadSettings.xml'
+  notifies :reload, 'service[jenkins]', :delayed
+end
+
+template "#{node['jenkins']['master']['home']}/jenkins.security.QueueItemAuthenticatorConfiguration.xml" do
+  source 'default/jenkins.security.QueueItemAuthenticatorConfiguration.xml'
+  notifies :reload, 'service[jenkins]', :delayed
+end
 
 jenkins_plugin 'greenballs'
 jenkins_plugin 'github'
@@ -14,10 +51,6 @@ jenkins_plugin 'cmakebuilder'
 jenkins_plugin 'xunit'
 jenkins_plugin 'mercurial'
 jenkins_plugin 'multiple-scms'
-
-secretsfilename = File.join(Chef::Config[:file_cache_path], 'cookbooks/aws_chef_jenkins/files/default/', node['secretsfilename'])
-secretsfile = File.read(secretsfilename)
-secretsobject = JSON.parse(secretsfile)
 
 serverinstance = search("aws_opsworks_instance", "self:true").first
 
